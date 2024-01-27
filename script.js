542e9664611ab50a0d72b4246400cead6c92b976
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-01-27 15:18:29 +0100

    array object with Roman numerals and Arabic Numerals value

---
+++ script.js
@@ -0,0 +1,36 @@
+//variables
+const inputNumber = document.getElementById("number")
+const convertBtn = document.getElementById("convert-btn")
+const output = document.getElementById("output")
+
+const romanConverter =[
+    {
+        value: 1000, romanNum: "M"
+    },
+    {
+        value: 900, romanNum: "CM"
+    },    {
+        value: 500, romanNum: "D"
+    },    {
+        value: 400, romanNum: "CD"
+    },    {
+        value: 100, romanNum: "C"
+    },    {
+        value: 90, romanNum: "XC"
+    },    {
+        value: 50, romanNum: "L"
+    },    {
+        value: 40, romanNum: "XL"
+    },    {
+        value: 10, romanNum: "X"
+    },    {
+        value: 9, romanNum: "IX"
+    },    {
+        value: 5, romanNum: "V"
+    },    {
+        value: 4, romanNum: "IV"
+    },    {
+        value: 1, romanNum: "I"
+    },
+]
+
