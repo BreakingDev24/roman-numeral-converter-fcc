8f19d44e2e6995c30321e9fc5d7142a86c36fe0c
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-01-29 23:09:39 +0100

    improved readability

--- script.js
+++ script.js
@@ -3,47 +3,21 @@ const inputNumber = document.getElementById("number")
 const convertBtn = document.getElementById("convert-btn")
 const output = document.getElementById("output")
 
-const numbersArabicToRoman=[
-    {
-        value: 1000, romanNum: "M"
-    },
-    {
-        value: 900, romanNum: "CM"
-    },    
-    {
-        value: 500, romanNum: "D"
-    },    
-    {
-        value: 400, romanNum: "CD"
-    },    
-    {
-        value: 100, romanNum: "C"
-    },    
-    {
-        value: 90, romanNum: "XC"
-    },    
-    {
-        value: 50, romanNum: "L"
-    },    
-    {
-        value: 40, romanNum: "XL"
-    },    
-    {
-        value: 10, romanNum: "X"
-    },    
-    {
-        value: 9, romanNum: "IX"
-    },    
-    {
-        value: 5, romanNum: "V"
-    },    
-    {
-        value: 4, romanNum: "IV"
-    },   
-    {
-        value: 1, romanNum: "I"
-    }
-]
+const numbersArabicToRoman = [
+    { value: 1000, romanNum: "M" },
+    { value: 900, romanNum: "CM" },
+    { value: 500, romanNum: "D" },
+    { value: 400, romanNum: "CD" },
+    { value: 100, romanNum: "C" },
+    { value: 90, romanNum: "XC" },
+    { value: 50, romanNum: "L" },
+    { value: 40, romanNum: "XL" },
+    { value: 10, romanNum: "X" },
+    { value: 9, romanNum: "IX" },
+    { value: 5, romanNum: "V" },
+    { value: 4, romanNum: "IV" },
+    { value: 1, romanNum: "I" }
+];
 
 function isValidInput(num){
     switch(true){
@@ -66,7 +40,7 @@ function getNumber() {
     const isValidResult = isValidInput(inputValue)
    
     output.classList.remove('output-hidden')
-    
+
     if(isValidResult === true){
         output.classList.remove('output-error')
         output.textContent = convertNumber(inputValue)
